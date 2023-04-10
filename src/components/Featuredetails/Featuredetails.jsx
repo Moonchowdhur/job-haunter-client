48ceacb5bc4c0c5594db5b83dcd92de8a84964bb
kaushani <kaushanichowdhury52@gmail.com>
2023-04-10 20:43:27 +0530

    details part

--- src/components/Featuredetails/Featuredetails.jsx
+++ src/components/Featuredetails/Featuredetails.jsx
@@ -1,5 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import {BiLocationPlus,BiRupee} from "react-icons/bi";
+import {MdSubtitlesOff} from "react-icons/md";
+import {BsFillTelephoneFill} from "react-icons/bs";
+import {AiOutlineMail} from "react-icons/ai";
+
+
 
 const Featuredetails = () => {
 
@@ -31,16 +37,48 @@ console.log(features)
         <div className='md:w-[30%] my-16 p-4 bg-[#FCC8D1] rounded-md mx-7 '>
             <h2 className='text-2xl my-5 font-bold'>Job Details</h2>
             <hr />
-            <p className='my-5'>Salary:{features?.salary}</p>
-            <p>{features?.name}</p>
-            <h2 className='text-2xl mt-8 mb-3 font-bold '>Contact Information</h2><hr />
-            <p>{features?.phone}</p>
-            <p>{features?.email}</p>
-            <p>{features?.address}</p>
-            <button className='bg-violet-500 text-center font-bold hover:bg-violet-700 text-white rounded-lg my-7 px-3 w-full py-2 text-2xl'>Apply Now</button>
+           <div className='flex my-5 items-center'>
+            <span className='text-3xl text-violet-500 font-bold'>
+            <BiRupee/>
+            </span>
+           <p className=''><span className='font-bold text-xl'>Salary:
+           </span>{features?.salary}(Per Month)</p>
+           </div>
+           <div className='flex items-center gap-2 '>
+           <span className='text-xl text-violet-500 font-bold'>
+            <MdSubtitlesOff/>
+            </span>
+           <p className=''><span className='font-bold text-xl'>Job Title:
+           </span>{features?.name}</p>
+          </div>
+          <h2 className='text-2xl mt-10 mb-3 font-bold '>Contact Information</h2><hr />
+          <div className='flex gap-2 mt-4 items-center'>
+          <span className='text-xl text-violet-500 font-bold'>
+            <BsFillTelephoneFill/>
+            </span>
+           <p className=''><span className='font-bold text-xl'>Phone:  
+           </span>{features?.phone}</p>
+          </div>
+          <div className='flex gap-2 mt-4 items-center'>
+          <span className='text-xl text-violet-500 font-bold'>
+            <AiOutlineMail/>
+            </span>
+           <p className=''><span className='font-bold text-xl'>Email:
+           </span>{features?.email}</p>
+          </div>
+          <div className='flex gap-2 mt-4 items-center'>
+          <span className='text-2xl text-violet-500 font-bold'>
+            <BiLocationPlus/>
+            </span>
+           <p className=''><span className='font-bold text-xl'>Address:
+           </span> {features?.address}</p>
+          </div>
+          <button className='bg-violet-500 text-center font-bold hover:bg-violet-700 text-white rounded-lg my-7 px-3 w-full py-2 text-2xl'>Apply Now</button>
         </div>
+        
         </div>
     );
 };
 
 export default Featuredetails;
+
